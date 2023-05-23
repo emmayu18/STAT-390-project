19e275fdd3638f904bbabdfb8bb5040af95d98a0
rxz5570 <rhonazhang2023@u.northwestern.edu>
2023-05-23 16:54:19 -0500

    map-shiny updated

--- map/app.R
+++ map/app.R
@@ -1,4 +1,7 @@
-# load in packages
+# My Chi My Future Shiny Application ---------------------------------------
+
+
+# load packages that will be used for the application
 library(shiny)
 library(tidyverse)
 library(dplyr)
@@ -11,103 +14,55 @@ library(mapview)
 library(leaflet)
 library(sp)
 
-# load in data
-
-data <- read_tsv("convert_MCMF_ALL_TIME_DATA.csv", show_col_types = FALSE) %>% 
-  clean_names() %>% 
-  # removing irrelevant columns
-  select(-c("index_row", "logo_url", "online_address", "program_url", 
-            "registration_url","contact_name", "contact_email", 
-            "contact_phone")) %>%
-  # renaming duplicate category names
-  mutate(category_name = str_replace(category_name, '&', 'And'),
-         category_name = str_replace_all(category_name, '\\.', '')) %>% 
-  # removing programs with min_age over 25
-  filter(min_age < 25)
-
+# load in data ------------------------------------------------------------------
 load("map_cleaning.rda")
-
-##### EXTRA WRANGLING
-# 
-# removed_programs <- data %>%
-#   filter((grepl("geocoding", description, ignore.case = TRUE) |
-#              grepl("test program", program_name, ignore.case = TRUE)))
-# 
-# 
-# # data w/ column for length of opportunity
-# time <- data %>%
-#   mutate(start_date = as.Date(start_date, format = "%m/%d/%y"),
-#          end_date = parse_date_time(end_date, orders = c("%m/%d/%y")),
-#          program_length = as.numeric(difftime(end_date, start_date, units = "days"))) %>% 
-#   mutate(program_length = ifelse(program_length == 0, 1, program_length))
-# 
-# # data w/ column for season of each event based on month of start_date
-# seasons <- data %>%
-#   mutate(new_start_date = parse_date_time(start_date, orders = c("%m/%d/%y")),
-#          program_season = case_when(
-#            month(new_start_date) %in% c(3,4,5) ~ 'spring',
-#            month(new_start_date) %in% c(6,7,8) ~ 'summer',
-#            month(new_start_date) %in% c(9,10,11) ~ 'fall',
-#            month(new_start_date) %in% c(12,1,2) ~ 'winter',
-#          )) %>% 
-#   select(-new_start_date)
-
-# addressing data points with multiple categories
-# data w/ categories for each program based on id
-# cat_duplicates <- data %>%
-#   group_by(id) %>%
-#   summarize(category_name = paste0(category_name, collapse = ", ")) %>%
-#   mutate(category_name = strsplit(category_name, ", "))
-# 
-# data_nodup <- data %>%
-#   select(-category_name) %>%
-#   distinct(id, .keep_all = TRUE) %>%
-#   inner_join(cat_duplicates, by = c("id"))
-
-## community count
-eda_counts <- mcmf_sp %>%
-  group_by(community, category_name) %>%
-  summarise(n = n()) %>% 
-  mutate(category_name = str_replace_all(category_name, '\\.', '')) %>% 
-  mutate(general_category = case_when(
-    category_name %in% c("Music & Art", "Performance", "Sports + Wellness", "Nature", "Food") ~ "Leisure & Arts",
-    category_name %in% c("Academic Support", "Math", "Reading & Writing", "Science", "Science & Math","Social Studies", "Teaching") ~ "Academics",
-    category_name %in% c("Building & Fixing Things", "Computers", "Digital Media", "Managing Money", "Law", "Work + Career") ~ "Professional Skill Building",
-    category_name %in% c("Helping Your Community", "Transportation", "Customer/Human Service", "Healthcare") ~ "Community Service"))
-
-gencat_count <- eda_counts %>%
-  group_by(community, general_category) %>%
-  summarize(n = sum(n))
+load("map_counts.rda")
 
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("My CHI. My Future Programs Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-          radioButtons("gen_category", 
-                             label = "Select a Category", 
-                             choices = c("Academics",
-                                         "Leisure & Arts",
-                                         "Professional Skill Building",
-                                         "Community Service"),
-                             selected = "Academics")
-        ),
-        
-        mainPanel(
-          tabsetPanel(
-            tabPanel("Map of Accessibility", plotOutput("map")),
-            tabPanel("Summary", "summary"),
-            tabPanel("Table", "table")
-          )
+# Set up the application ui ----------------------------------------
+ui <- shinyUI(navbarPage("My CHI. My Future. Explorer",
+    
+    # define the tabs to be used in the app
+    # introduction 
+    tabPanel("Intro",
+             includeMarkdown("intro.Rmd"),
+             hr()),
+    
+    tabPanel("Map",
+             fluidRow(column(12,
+                             h1("Map of Accessibility to Programs"),
+                             p("How accessible are different categories of programs for Chicago's different communities/regions?"),
+                             br(),
+                             br(),
+                             h4("Note the topics included in each category:"),
+                             strong("Academics:"), "Academic Support, Math, Reading & Writing, Science, Science & Math, Social Studies, and Teaching",
+                             br(),
+                             strong("Leisure & Arts:"), "Music & Art, Performance, Sports + Wellness, Nature, and Food",
+                             br(),
+                             strong("Professional Skill Building:"), "Building & Fixing Things, Computers, Digital Media, Managing Money, Law, and Work + Career",
+                             br(),
+                             strong("Community Service:"), "Helping Your Community, Transportation, Customer/Human Service, and Healthcare",
+             )),
+             hr(),
+             fluidRow(sidebarPanel(width = 3,
+                                   h4("Select a Category:"),
+                                   helpText("Chose a category to look at its accessibility in your community"),
+                                   radioButtons("gen_category", 
+                                                label = "Select a Category:", 
+                                                choices = c("Academics",
+                                                            "Leisure & Arts",
+                                                            "Professional Skill Building",
+                                                            "Community Service"),
+                                                selected = "Academics")),
+                      mainPanel(plotOutput("map", height = 500))
+                      ))
+    
+    # close the UI definition
     ))
-)
 
-# Define server logic required to draw a histogram
+
+
+# Set up the application server ----------------------------------------
 server <- function(input, output) {
 
     output$map <- renderPlot({
@@ -123,10 +78,10 @@ server <- function(input, output) {
         filter(general_category == input$gen_category) %>% 
         ggplot() +
         geom_sf(aes(fill = n)) +
-        scale_fill_gradient(low = "white", high = "blue") +
+        scale_fill_gradient(name = "Count", low = "white", high = "#FF0000") +
         theme_void()
     })
 }
 
-# Run the application 
+# run the application  ----------------------------------------
 shinyApp(ui = ui, server = server)
