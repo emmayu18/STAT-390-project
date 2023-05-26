6168842d8ac50655e969791c46ea7aee56a0d07d
rxz5570 <rhonazhang2023@u.northwestern.edu>
2023-05-25 20:30:06 -0500

    shiny changes - including emmas

--- map/app.R
+++ map/app.R
@@ -97,7 +97,15 @@ ui <- shinyUI(
                                             selectInput("feature",
                                                         label = "",
                                                         choices = c("Number of Programs",
-                                                                    "Programs that offer free food"))
+                                                                    "Programs that offer free food")),
+                                            h4("Select Grade Level Range:"),
+                                            helpText("Select the grade level range you want to see. 0 stands for pre-K and below, and 13 stands for college and above."),
+                                            sliderInput("age_range",
+                                                        label = "",
+                                                        min = 0,
+                                                        max = 13,
+                                                        value = c(0, 13),
+                                                        step = 1)
                       ),
                       mainPanel(plotOutput("map", height = 500)),
                       # add the overlay panel
@@ -141,7 +149,7 @@ ui <- shinyUI(
                                mainPanel(plotOutput("bar")))),
              
              # recommendations to stakeholders ----------------------------------------
-             tabPanel("Observations",
+             tabPanel("Recommendations"
                       # includeMarkdown("intro.Rmd")
                       ),
                       
@@ -181,6 +189,54 @@ ui <- shinyUI(
       data_type <- switch(input$data_type,
                           "Count" = "",
                           "Proportion" = "prop_")
+      
+      # wrangle data
+      eda_counts2 <- eda_counts
+      gencat_count <- as.data.frame(eda_counts) %>%
+        filter(!(min_grade < input$age_range[1] & max_grade < input$age_range[1]) & 
+                 !(min_grade > input$age_range[2] & max_grade > input$age_range[2])) %>%
+        group_by(community, general_category) %>%
+        summarize(n = n(),
+                  free_food = sum(program_provides_free_food)) 
+      
+      gencat_count <- gencat_count %>%
+        group_by(community, general_category) %>%
+        summarize(n = sum(n),
+                  free_food = sum(free_food)) %>% 
+        inner_join(as.data.frame(eda_counts2) %>% select(the_geom, community) %>% distinct(), by = "community")
+      
+      gencat_count <- pivot_wider(data = as.data.frame(gencat_count),
+                                  names_from = general_category,
+                                  values_from = c(n, free_food, the_geom)) %>%
+        select(-c(
+          `the_geom_Leisure & Arts`,
+          `the_geom_Professional Skill Building`,
+          `the_geom_Community Service`)) %>%
+        rename("the_geom" = "the_geom_Academics") %>%
+        mutate(n_Academics = replace_na(n_Academics, 0),
+               `n_Community Service` = replace_na(`n_Community Service`, 0),
+               `n_Professional Skill Building` = replace_na(`n_Professional Skill Building`, 0),
+               `n_Leisure & Arts` = replace_na(`n_Leisure & Arts`, 0),
+               free_food_Academics = replace_na(free_food_Academics, 0),
+               `free_food_Community Service` = replace_na(`free_food_Community Service`, 0),
+               `free_food_Professional Skill Building` = replace_na(`free_food_Professional Skill Building`, 0),
+               `free_food_Leisure & Arts` = replace_na(`free_food_Leisure & Arts`, 0)) %>%
+        mutate(n_sum = n_Academics + `n_Community Service` +
+                 `n_Professional Skill Building` + `n_Leisure & Arts`,
+               free_food_sum = free_food_Academics + `free_food_Community Service` +
+                 `free_food_Professional Skill Building` + `free_food_Leisure & Arts`) %>%
+        mutate(n_prop_Academics = n_Academics / n_sum,
+               `n_prop_Community Service` = `n_Community Service` / n_sum,
+               `n_prop_Leisure & Arts` = `n_Leisure & Arts` / n_sum,
+               `n_prop_Professional Skill Building` = `n_Professional Skill Building` / n_sum,
+               free_food_prop_Academics = free_food_Academics / n_Academics,
+               `free_food_prop_Community Service` = `free_food_Community Service` / `n_Community Service`,
+               `free_food_prop_Leisure & Arts` = `free_food_Leisure & Arts` / `n_Leisure & Arts`,
+               `free_food_prop_Professional Skill Building` = `free_food_Professional Skill Building` / `n_Professional Skill Building`) %>%
+        mutate(free_food_prop_Academics = replace_na(free_food_prop_Academics, 0),
+               `free_food_prop_Community Service` = replace_na(`free_food_prop_Community Service`, 0),
+               `free_food_prop_Leisure & Arts` = replace_na(`free_food_prop_Leisure & Arts`, 0),
+               `free_food_prop_Professional Skill Building` = replace_na(`free_food_prop_Professional Skill Building`, 0))
       
       # filter based on selected categories
       st_as_sf(gencat_count) %>%
